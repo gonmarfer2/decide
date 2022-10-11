2abfe36574fff2531fc5594dfa0b398366047589
gonmarfer2 <gonmarfer2@alum.us.es>
2022-10-11 11:42:40 +0200

    Deleted testSelenium.py until I know how to handle it

--- decide/census/admin.py
+++ decide/census/admin.py
@@ -4,10 +4,10 @@ from .models import Census
 
 
 class CensusAdmin(admin.ModelAdmin):
-    list_display = ('voting_id', 'voter_id','adscription')
-    list_filter = ('voting_id','adscription' )
+    list_display = ('voting_id', 'voter_id')
+    list_filter = ('voting_id', )
 
-    search_fields = ('voter_id', 'adscription' )
+    search_fields = ('voter_id', )
 
 
 admin.site.register(Census, CensusAdmin)
